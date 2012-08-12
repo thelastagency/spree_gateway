4de2f16299624758029ca4a9f04812a47a55937e
lostboy <paul.crabtree@gmail.com>
2012-08-12 18:58:51 +0100

    cardsave gateway

---
+++ app/models/spree/gateway/card_save.rb
@@ -0,0 +1,13 @@
+module Spree
+  class Gateway::CardSave < Gateway
+    preference :login, :string
+    preference :password, :string
+    
+    attr_accessible :preferred_login, :preferred_password
+
+    def provider_class
+      ActiveMerchant::Billing::CardSaveGateway
+    end
+
+  end
+end
